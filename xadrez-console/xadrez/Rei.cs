bbbb007ed5bc97a72a9254bb270e03abd29383b4
Daniel <danielcarmo2@gmail.com>
2024-08-07 09:02:10 -0300

    comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tabuleiro;
+
+namespace xadrez
+{
+     class Rei:Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor){
+
+        }
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
