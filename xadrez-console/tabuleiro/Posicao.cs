e2e6fe40165a1decf1ec3b0d426b6e7e6dc00474
Daniel <danielcarmo2@gmail.com>
2024-08-08 11:13:58 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -2,18 +2,26 @@
 {
     internal class Posicao
     {
-        public int linha {get; set; }
-        public int coluna {get; set;}
+        public int linha { get; set; }
+        public int coluna { get; set; }
 
-        public Posicao(int linha, int coluna) {
-            
+        public Posicao(int linha, int coluna)
+        {
+
             this.linha = linha;
             this.coluna = coluna;
         }
+
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
         public override string ToString()
         {
-            return linha 
-                + ", " 
+            return linha
+                + ", "
                 + coluna;
         }
     }
